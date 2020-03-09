60152add0e4b715713df92416edb00c23826639b
Melike KILIC <melike180400@gmail.com>
2020-03-09 21:29:32 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,8 +14,9 @@ def shipping_manifest
 the_manifest = {
                 "whale bone corsets"=> 5, 
                "porcelain vases" => 2, 
-                "oil paintings" => 3}
-               return the_manifest
+                "oil paintings" => 3
+               }
+return the_manifest
 
 end
 
